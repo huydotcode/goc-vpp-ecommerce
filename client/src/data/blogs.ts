f980f9106ab05730581829908d6f5ad64428719a
Ngô Huy <117896799+huydotcode@users.noreply.github.com>
2025-12-10 00:32:30 +0700

    feat: article blogs remove tags, comment section, fix path .html

--- client/src/data/blogs.ts
+++ client/src/data/blogs.ts
@@ -15,7 +15,8 @@ export const blogData: BlogPost[] = [
       "Khám phá 5 mẹo ghi chép thông minh giúp bạn học nhanh nhớ lâu trong mùa thi cùng bút và tập Thiên Long.",
     slug: "5-meo-ghi-chep-thong-minh-giup-ban-nho-lau-hon-trong-mua-thi",
     author: "Góc VPP",
-    fileName: "5-meo-ghi-chep-thong-minh-giup-ban-nho-lau-hon-trong-mua-thi.html"
+    fileName:
+      "5-meo-ghi-chep-thong-minh-giup-ban-nho-lau-hon-trong-mua-thi.html",
   },
   {
     title:
@@ -25,7 +26,8 @@ export const blogData: BlogPost[] = [
       "Khám phá các phương pháp ghi chép hiệu quả như Cornell, sơ đồ tư duy, Bullet Journal… giúp bạn học tập và làm việc thông minh hơn.",
     slug: "phuong-phap-ghi-chep-hieu-qua-bi-quyet-hoc-tap-va-lam-viec-thong-minh",
     author: "Góc VPP",
-    fileName: "phuong-phap-ghi-chep-hieu-qua-bi-quyet-hoc-tap-va-lam-viec-thong-minh.html"
+    fileName:
+      "phuong-phap-ghi-chep-hieu-qua-bi-quyet-hoc-tap-va-lam-viec-thong-minh.html",
   },
   {
     title: "Khám phá điểm đặc biệt thường bị ngó lơ của bút",
@@ -34,7 +36,7 @@ export const blogData: BlogPost[] = [
       "Trong vô vàn lựa chọn bút viết trên thị trường, bạn chắc hẳn đã chú ý đến màu mực, hình dáng hay kích thước ngòi bút. Nhưng có một chi tiết rất quan trọng mà ít người để ý đó chính là đầu bút.",
     slug: "kham-pha-diem-dac-biet-thuong-bi-ngo-lo-cua-but",
     author: "Góc VPP",
-    fileName: "kham-pha-diem-dac-biet-thuong-bi-ngo-lo-cua-but"
+    fileName: "kham-pha-diem-dac-biet-thuong-bi-ngo-lo-cua-but.html",
   },
   {
     title:
@@ -44,7 +46,8 @@ export const blogData: BlogPost[] = [
       "Ở lứa tuổi cấp 1 - cấp 2, trẻ em rất thích được vừa học vừa chơi. Đồ dùng học tập như bút màu, bút viết, giấy, tập… không chỉ giúp kích thích tư duy, phát triển trí tuệ, mà còn góp phần tạo niềm vui cho việc học.",
     slug: "chon-dung-cu-hoc-tap-an-toan-cho-tre-nhung-tieu-chuan-an-toan-quoc-te",
     author: "Góc VPP",
-    fileName: "chon-dung-cu-hoc-tap-an-toan-cho-tre-nhung-tieu-chuan-an-toan-quoc-te"
+    fileName:
+      "chon-dung-cu-hoc-tap-an-toan-cho-tre-nhung-tieu-chuan-an-toan-quoc-te.html",
   },
   {
     title: "Top 5 màu sắc dành cho học sinh cấp 2",
@@ -53,6 +56,6 @@ export const blogData: BlogPost[] = [
       "Tuổi cấp 2 là giai đoạn các bạn nhỏ bắt đầu bước vào thế giới hội họa một cách nghiêm túc hơn. Để đồng hành cùng con trong hành trình đó, việc chọn đúng loại màu nước là vô cùng quan trọng.",
     slug: "top-5-mau-sac-danh-cho-hoc-sinh-cap-2",
     author: "Góc VPP",
-    fileName: "top-5-mau-sac-danh-cho-hoc-sinh-cap-2"
+    fileName: "top-5-mau-sac-danh-cho-hoc-sinh-cap-2.html",
   },
 ];
